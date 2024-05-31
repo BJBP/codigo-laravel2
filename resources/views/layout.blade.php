d48e1e822754765e6ee99c261289dd864c83ab5d
BJBP <jaison.03042000@gmail.com>
2024-05-31 17:09:41 -0500

    Navegación de Página

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<title>@yield('title')</title>
+</head>
+<body>
+<h1>Home</h1>
+<ul>
+<li><a href="/">Home</a></li>
+<li><a href="nosotros">Nosotros</B></li>
+<li><a href="servicios">Servicios</a></li>
+<li><a href="contacto">Contacto</a></li>
+</ul>
+@yield('content')
